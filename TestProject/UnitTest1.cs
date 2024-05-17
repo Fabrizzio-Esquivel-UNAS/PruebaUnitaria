14052e46a53021065eab1fc992c09e7584c72915
Fabrizzio Fabiano Esquivel Mori <fabrizzio.esquivel@unas.edu.pe>
2024-05-17 16:04:01 -0500

    Fix

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -9,7 +9,18 @@ namespace TestProject
         [TestMethod]
         public void TestMethod1()
         {
-
+            {
+                double result = calculateTotalPrice(-1, "standard", -1);
+                Assert.AreEqual(5.0, result);
+            }
+            {
+                double result = calculateTotalPrice(10, "standard", 20);
+                Assert.AreEqual(20.0, result);
+            }
+            {
+                double result = calculateTotalPrice(10, "standard", -1);
+                Assert.AreEqual(15.0, result);
+            }
             {
                 double result = calculateTotalPrice(60, "overnight", 5);
                 Assert.AreEqual(70, result);
